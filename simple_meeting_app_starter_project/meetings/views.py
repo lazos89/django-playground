d721e6a52376f28af6982d61ecd53e254ad1dfa3
Lazos Doug <lazarosd89@gmail.com>
2020-07-26 16:53:05 +0300

    [Meetings] Add basic tempaltes

--- simple_meeting_app_starter_project/meetings/views.py
+++ simple_meeting_app_starter_project/meetings/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting
 
-# Create your views here.
+
+def detail(request, id):
+    print(90 * "-")
+    meeting = get_object_or_404(Meeting, pk=10)
+    print(request)
+    print(id)
+    print(90 * "-")
+    return render(request, "meetings/detail.html", {"meeting": meeting})
--- simple_meeting_app_starter_project/website/views.py
+++ simple_meeting_app_starter_project/website/views.py
@@ -1,10 +1,14 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 
+from meetings.models import Meeting
+
 
 def welcome(request):
-    return HttpResponse("Welcome to Homee  Page")
+    return render(
+        request, "website/welcome.html", {"num_meetings": Meeting.objects.count()}
+    )
 
 
 def date(request):
