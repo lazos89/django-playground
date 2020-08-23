a6c3832a1911d1b547f8a25628cd3ac41d2e39d4
Lazos Doug <lazarosd89@gmail.com>
2020-08-23 17:57:12 +0300

    [Intro drf] finish project

---
+++ intro_dfr_example/products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import manufacturer_list, manufacturer_detail, product_list, product_detail
+
+urlpatterns = [
+    path("manufacturers/", manufacturer_list, name="manufacturer-list"),
+    path("manufacturers/<int:pk>/", manufacturer_detail, name="manufacturer-detail"),
+    path("products/", product_list, name="product-list"),
+    path("products/<int:pk>/", product_detail, name="product-detail"),
+]
